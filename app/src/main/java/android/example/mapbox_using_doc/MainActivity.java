afad37647ad3729484a119c02ce334ae88e287c5
aryan_0077 <rocking.am12459@gmail.com>
2019-09-10 21:58:10 +0530

    Permissions Granting & Loading Of Map

--- app/src/main/java/android/example/mapbox_using_doc/MainActivity.java
+++ app/src/main/java/android/example/mapbox_using_doc/MainActivity.java
@@ -7,8 +7,10 @@ import android.os.Bundle;
 import android.view.View;
 
 import com.mapbox.android.core.permissions.PermissionsListener;
+import com.mapbox.android.core.permissions.PermissionsManager;
 import com.mapbox.mapboxsdk.Mapbox;
 import com.mapbox.mapboxsdk.geometry.LatLng;
+import com.mapbox.mapboxsdk.location.LocationComponent;
 import com.mapbox.mapboxsdk.maps.MapView;
 import com.mapbox.mapboxsdk.maps.MapboxMap;
 import com.mapbox.mapboxsdk.maps.OnMapReadyCallback;
@@ -22,6 +24,7 @@ public class MainActivity extends AppCompatActivity implements OnMapReadyCallbac
 
     private MapView mapView;
     private MapboxMap mapboxMap;
+    private LocationComponent locationComponent;
 
 
     @Override
@@ -59,7 +62,24 @@ public class MainActivity extends AppCompatActivity implements OnMapReadyCallbac
             // When the map is Ready we need to display user Current Location
             this.mapboxMap = mapboxMap;
 
-            // We need to set the Style of map Box Map
-        
+            // We need to set the Style of map Box Map ( Leave it for Later )
+            mapboxMap.setStyle(getString(R.string.navigation_guidance_day), new Style.OnStyleLoaded() {
+                @Override
+                public void onStyleLoaded(@NonNull Style style) {
+                    enableLocationComponent(style);
+                }
+            });
+
+
+
+    }
+
+    private void enableLocationComponent(@NonNull Style loadedMapStyle) {
+        // Check If the Permissions are Enabled , if Not then Request for it
+        if(PermissionsManager.areLocationPermissionsGranted(this)) {
+            // Then we can Activate MapBox Location Component
+            locationComponent = mapboxMap.getLocationComponent();
+            locationComponent.activateLocationComponent(this,loadedMapStyle);
+        }
     }
 }
